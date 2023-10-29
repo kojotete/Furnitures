5c9a43c4aa82f58894a6f203f789637f7051c36f
kojotete <tommyniiwilliams@gmail.com>
2023-10-29 01:18:21 +0000

    added

--- JS/payment.js
+++ JS/payment.js
@@ -8,7 +8,7 @@ function payWithPaystack(e) {
     email: document.getElementById("email-address").value,
     amount: document.getElementById("amount").value * 100,
     currency: "GHS" & "USD",
-    ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+    ref: ''+Math.floor((Math.random() * 10000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
     // label: "Optional string that replaces customer email"
     onClose: function(){
       alert('Window closed.');
